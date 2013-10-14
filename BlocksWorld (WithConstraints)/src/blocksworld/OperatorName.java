56ebc6bb6a86b47d05ec26681a069070a341ea2c
Psicowired <tgq.spm@gmail.com>
2013-10-14 13:15:08 +0200

    Update OperatorName.java

--- BlocksWorld (WithConstraints)/src/blocksworld/OperatorName.java
+++ BlocksWorld (WithConstraints)/src/blocksworld/OperatorName.java
@@ -4,7 +4,7 @@ public enum OperatorName
 {
 // enum class of the operators
 /////////////////
-// Author: 'Toño G. Quintela' tgq.spm@gmail.com with the unpayable help of C.Levinas
+// Author: 'Toño G. Quintela' tgq.spm@gmail.com with the unaffordable help of C.Levinas
 
 	pick_up,
 	leave,
