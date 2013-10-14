84cf04b36d5bc45fc23b0489045b158330dcac12
Psicowired <tgq.spm@gmail.com>
2013-10-14 13:13:16 +0200

    Update PrintOutput.java

--- BlocksWorld (NoConstraints)/src/blocksworld/PrintOutput.java
+++ BlocksWorld (NoConstraints)/src/blocksworld/PrintOutput.java
@@ -15,7 +15,7 @@ public class PrintOutput
 // Outputs:
 //	** Print in screen
 /////////////////
-// Author: 'Toño G. Quintela' tgq.spm@gmail.com with the unpayable help of C.Levinas
+// Author: 'Toño G. Quintela' tgq.spm@gmail.com with the unaffordable help of C.Levinas
 
 
 
